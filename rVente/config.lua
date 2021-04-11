0ddfd5486c9f472766da201a3a7143a5fa3e46dd
Memeldu72 <63606977+Memeldu72@users.noreply.github.com>
2021-04-11 20:11:20 +0200

    Add files via upload

---
+++ rVente/config.lua
@@ -0,0 +1,45 @@
+Config = {
+    Categories = {
+        Pistol = {
+            {name = "WEAPON_PISTOL", label = "~b~Pistolet", price = 25000},
+            {name = "WEAPON_PISTOL_MK2", label = "~y~Pistolet MK2", price = 35000},
+            {name = "WEAPON_COMBATPISTOL", label = "~b~Pistolet de combat", price = 40000},
+            {name =  "WEAPON_PISTOL50", label = "~y~D.50", price = 50000}
+        },
+    
+        Mitraillettes = {
+    
+            {name = "WEAPON_COMBATPDW", label = "~y~ADP", price = 100000},
+            {name = "WEAPON_MACHINEPISTOL", label = "~b~Tec 9 ", price = 110000},
+            {name = "WEAPON_MINISMG", label = "~y~Scorpion ", price = 110000},
+            {name = "WEAPON_SMG_MK2", label = "~b~SMG MK2", price = 130000},
+            {name = "WEAPON_MICROSMG", label = "~y~UZI", price = 150000}
+        },
+    
+        Pompes = {
+    
+            {name = "WEAPON_SAWNOFFSHOTGUN", label = "~b~Sawn Off Shot Gun", price = 125000},
+            {name = "WEAPON_PUMPSHOTGUN", label = "~y~Fusil à pompe", price = 135000},
+            {name = "WEAPON_AUTOSHOTGUN", label = "~b~Pompe Auto ", price = 170000},
+            {name = "WEAPON_ASSAULTSHOTGUN", label = "~y~Pompe d'assaut", price = 230000}
+    
+        },
+    
+        Fusils = {
+    
+            {name = "WEAPON_COMPACTRIFLE", label = "~y~Mini-AK", price = 200000},
+            {name = "WEAPON_ASSAULTRIFLE", label = "~b~Ak-47", price = 280000},
+            {name = "WEAPON_CARBINERIFLE", label = "~y~M4 ", price = 335000}
+    
+        }
+    },
+
+    Position = {
+        Shops = {
+            {pos = vector3(580.651, -3111.383, 6.069), pedPos = vector3(582.31, -3110.76, 5.07), heading = 92.95, pedModel = "u_m_y_militarybum"}
+            
+        }
+    }
+    
+}
+
